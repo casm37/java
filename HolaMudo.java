84f44b4bd3d194d1deba2f4eb0900e390cbd68c3
casm37 <65574265+casm37@users.noreply.github.com>
2020-05-18 18:42:51 -0500

    Update HolaMudo.java

--- HolaMudo.java
+++ HolaMudo.java
@@ -3,13 +3,11 @@ public class HolaMudo {
     
     public static void main (String args[]){
         String saludar = "saludar";
-        
-        System.out.println(saludar);
         System.out.println(saludar);
         
         String despedirse = "adios";
-        
         System.out.println(despedirse);
         
+        System.out.println("Hola mundo");
     }
 }
