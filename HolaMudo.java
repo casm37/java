ae7673beb85076c151462305856273b8c73d17b3
casm37 <65574265+casm37@users.noreply.github.com>
2020-05-18 18:41:20 -0500

    Add files via upload

---
+++ HolaMudo.java
@@ -0,0 +1,15 @@
+
+public class HolaMudo {
+    
+    public static void main (String args[]){
+        String saludar = "saludar";
+        
+        System.out.println(saludar);
+        System.out.println(saludar);
+        
+        String despedirse = "adios";
+        
+        System.out.println(despedirse);
+        
+    }
+}
